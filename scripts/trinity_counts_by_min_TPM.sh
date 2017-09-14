8f9d3381971aaf740349a1f5feb0cbc8309fb8f1
sarahinwood <sninwood@gmail.com>
2017-09-14 14:23:59 +1200

    Delete trinity_counts_by_min_TPM.sh

--- scripts/trinity_counts_by_min_TPM.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bin/trinity/util/misc/count_matrix_features_given_MIN_TPM_threshold.pl \
-output/trinity_abundance/RSEM.TPM.not_cross_norm \
->output/trinity_abundance/RSEM.TPM.not_cross_norm.counts_by_min_TPM
