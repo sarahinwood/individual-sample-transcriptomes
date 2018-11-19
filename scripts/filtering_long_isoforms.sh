5328fbaefc2c83c41d49854958926dd9886cd511
sarahinwood <sninwood@gmail.com>
2018-11-19 18:42:38 +1300

    Delete filtering_long_isoforms.sh

--- scripts/filtering_long_isoforms.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-bin/bbmap/filterbyname.sh \
-in=output/trinity/Trinity.fasta \
-include=t \
-names=output/length_filtered/transcript_ids_above_500.txt \
-out=output/length_filtered/longest_transcripts_above_500.fasta
